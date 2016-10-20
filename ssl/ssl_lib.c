e6bea33dc72bf58c26d8786adb764d28d0565b7d
Chanho Park <chanho61.park@samsung.com>
2016-10-20 20:26:38 +0900

    apply openssl-1.0.2g-disable-sslv2v3.patch
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -2064,11 +2064,11 @@ SSL_CTX *SSL_CTX_new(const SSL_METHOD *meth)
     ret->options |= SSL_OP_LEGACY_SERVER_CONNECT;
 
     /*
-     * Disable SSLv2 by default, callers that want to enable SSLv2 will have to
-     * explicitly clear this option via either of SSL_CTX_clear_options() or
+     * Disable SSLv2 and SSLv3 by default, callers that want to enable these will have to
+     * explicitly clear these options via either of SSL_CTX_clear_options() or
      * SSL_clear_options().
      */
-    ret->options |= SSL_OP_NO_SSLv2;
+    ret->options |= SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3;
 
     return (ret);
  err:
